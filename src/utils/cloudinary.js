68ec2738f1393765cc91aa37477eaca46bb73aef
abhinawsingh8235 <abhinawsingh8235@gmail.com>
2026-04-02 02:02:04 +0530

    connect to mongo atlas

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs"
 
-(async function() {
 
     // Configuration
     cloudinary.config({ 
@@ -15,16 +14,19 @@ import fs from "fs"
         try {
             if (!localFilePath) return null
             // upload the file on cloudinary
-            const response = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto"})
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: "auto" 
+            })
             // file has been uploaded successfully
             console.log("file is upload on cloudinary", response.url);
+            fs.unlinkSync(localFilePath)
             return response;
+
         } catch (error) {
             fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operationn got failed
             return null;
         }
     }
+   
 
-})    
-
-  export {uploadOnCloudinary};
+  export { uploadOnCloudinary };
