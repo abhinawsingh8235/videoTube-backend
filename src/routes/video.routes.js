a14d0c27453010e37a8b3a57455497d98f90bc9d
abhinawsingh8235 <abhinawsingh8235@gmail.com>
2026-07-14 17:50:02 +0530

    Refactor video.routes.js for consistent formatting and apply verifyJWT middleware to all routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
     deleteVideo,
     getAllVideos,
@@ -6,37 +6,43 @@ import {
     publishAVideo,
     togglePublishStatus,
     updateVideo,
-} from "../controllers/video.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
-import {upload} from "../middlewares/multer.middleware.js"
+} from "../controllers/video.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
-    .route("/")
-    .get(getAllVideos)
-    .post(
-        upload.fields([
-            {
-                name: "videoFile",
-                maxCount: 1,
-            },
-            {
-                name: "thumbnail",
-                maxCount: 1,
-            },
-            
-        ]),
-        publishAVideo
-    );
+.route("/")
+.get(getAllVideos)
+.post(
+    verifyJWT,
+    upload.fields([
+        {
+            name: "videoFile",
+            maxCount: 1,
+        },
+        {
+            name: "thumbnail",
+            maxCount: 1,
+        },
+    ]),
+    publishAVideo
+);
 
 router
-    .route("/:videoId")
-    .get(getVideoById)
-    .delete(deleteVideo)
-    .patch(upload.single("thumbnail"), updateVideo);
+.route("/:videoId")
+.get(getVideoById)
+.delete(verifyJWT, deleteVideo)
+.patch(
+    verifyJWT,
+    upload.single("thumbnail"),
+    updateVideo
+);
 
-router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+router
+.route("/toggle/publish/:videoId")
+.patch(verifyJWT, togglePublishStatus);
 
-export default router
+export default router;
